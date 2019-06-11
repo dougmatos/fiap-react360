d81253366595494d31bfa5a8771149f2854a9340
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-10 22:50:56 -0300

    aula 1

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  VrButton
 } from 'react-360';
 
 export default class Hello360 extends React.Component {
+
+
+  state = {
+    count: 0
+  };
+
+  _increment = () => this.setState({count: this.state.count + 1});
+  _decrement = () => this.setState({count: this.state.count - 1});
 
   render() {
     return (
       <View style={styles.panel}>
         <Text style={styles.greeting}>
-            Simple Example with React 360
+            {`count: ${this.state.count}`}
         </Text>
+        
+        <VrButton onClick={this._increment} style={styles.greetingBox}>
+          <Text>+ ADD</Text>
+        </VrButton>
+        <VrButton onClick={this._decrement} style={styles.greetingBox}>
+          <Text>- REMOVE</Text>
+        </VrButton>
       </View>
     );
   }
@@ -23,12 +39,21 @@ const styles = StyleSheet.create({
   panel: {
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 0, 0, 1.0)',
+    backgroundColor: 'rgba(0, 150, 150, .5)',
     justifyContent: 'center',
     alignItems: 'center',
   },
   greeting: {
     fontSize: 48,
+    margin:'5 auto',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#ffffff',
+    borderWidth: 2,
+    width:200,
+    margin:10
   },
 });
 
